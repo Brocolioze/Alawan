2b1731906d8ccdac6b7cf9da7b2b183c46c13805
thony311 <antoinelefebvrepoupa@hotmail.com>
2024-03-18 08:35:49 -0400

    Update api.php

--- AlawanLaravel/routes/api.php
+++ AlawanLaravel/routes/api.php
@@ -46,7 +46,7 @@ Route::post('/login', [PersonController::class, 'login']);
 Route::post('/person', [PersonController::class, 'addPerson']);
 Route::delete('/person/{id}', [PersonController::class, 'deletePerson']);
 Route::get('/persons', function(){ return PersonResource::collection(Person::all()); });
-Route::get('/getAuth', [PersonController::class, 'getIdAuth']);
+Route::get('/getIdAuth', [PersonController::class, 'getIdAuth']);
 Route::middleware('auth:api')->get('/persons', [PersonController::class, 'getAllPersons']);
 Route::middleware('auth:api')->get('/user', [PersonController::class, 'getLoggedInPerson']);
 Route::middleware('auth:api')->post('/logout', [PersonController::class, 'logout']);
